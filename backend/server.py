65b2fbd9781824255c19a1a9e2c91eefe9ce9a35
RaiFernandesdosSantos <raifernandes076@gmail.com>
2025-01-03 16:07:57 -0400

    teste imagens

--- backend/server.py
+++ backend/server.py
@@ -1,30 +1,20 @@
-from flask import Flask, request, jsonify
-import os
+# from flask import Flask, request
+# import os
+# import cv2
+# import pytesseract
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify({"message": "Servidor Flask está ativo e funcionando!"})
+# @app.route("/lerfoto", methods=["POST"])
+# def lerfoto():
+#     file = request.files["file"]
+#     imagem = cv2.imread(file)
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'image' not in request.files:
-        app.logger.info("Nenhum arquivo encontrado na solicitação.")
-        return 'No file part', 400
-    file = request.files['image']
-    if file.filename == '':
-        app.logger.info("Nenhum arquivo selecionado.")
-        return 'No selected file', 400
-    if file:
-        filepath = os.path.join(UPLOAD_FOLDER, file.filename)
-        file.save(filepath)
-        app.logger.info(f"Arquivo recebido: {file.filename}")
-        app.logger.info(f"Arquivo salvo em {filepath}")
-        return 'File uploaded successfully', 200
+import cv2
+import pytesseract
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+image = cv2.imread("./assets/testando.png")
+
+text = pytesseract.image_to_string(image)
+print(text)
