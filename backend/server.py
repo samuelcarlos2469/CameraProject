53433e86901645350aa34654cdaf40d101578213
RaiFernandesdosSantos <raifernandes076@gmail.com>
2025-01-20 13:29:12 -0400

    agora deu certo

--- backend/server.py
+++ backend/server.py
@@ -1,20 +1,36 @@
-# from flask import Flask, request
-# import os
-# import cv2
-# import pytesseract
-
-# app = Flask(__name__)
-
-
-# @app.route("/lerfoto", methods=["POST"])
-# def lerfoto():
-#     file = request.files["file"]
-#     imagem = cv2.imread(file)
-
 import cv2
-import pytesseract
+import PIL.Image
+import google.generativeai as genai
+from docx import Document
+from flask import Flask
 
-image = cv2.imread("./assets/testando.png")
 
-text = pytesseract.image_to_string(image)
-print(text)
+genai.configure(api_key="AIzaSyDqxdaEYCu3z727HXeJx3VRwyjeWkuV7e8")
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+image_path = "./assets/images.jpg"
+image = cv2.imread(image_path)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+_, thresh = cv2.threshold(gray, 102, 255, cv2.THRESH_BINARY_INV)
+
+cv2.imwrite("imagem_processada.png", thresh)
+
+
+text = PIL.Image.open("imagem_processada.png")
+response = model.generate_content(
+    ["O que está escrito? apenas transcreva o texto sem comentar nada", text]
+)
+print(response.text)
+
+doc = Document()
+style = doc.styles["Normal"]
+font = style.font
+font.name = "Times New Roman"
+text_doc = response.text
+
+doc.add_paragraph(text_doc)
+
+doc.save("document.docx")
+
+print("Convertido com sucesso!")
